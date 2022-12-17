2ab96a78b5e2117d07cba119f3a639fab92c6387
Shruti3004 <mail2shruti.ag@gmail.com>
2022-12-17 16:50:45 +0530

    feat: chapi functions
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import "./App.css";
+// import * as CredentialHandlerPolyfill from "credential-handler-polyfill";
+// import * as WebCredentialHandler from "web-credential-handler";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const getWallet = async () => {
+      // await CredentialHandlerPolyfill.loadOnce();
+      // console.log("Ready to work with credentials!");
+      const polyfill = await window.credentialHandlerPolyfill.loadOnce();
+      const { CredentialManager, CredentialHandlers } = polyfill;
+
+      const result = await CredentialManager.requestPermission();
+      // if (result !== "granted") {
+      //   throw new Error("Permission denied.");
+      // }
+
+      console.log("polyfill", polyfill);
+      console.log("result", result);
+
+      // await WebCredentialHandler.installHandler();
+      // console.log("Wallet installed.");
+    };
+    getWallet();
+  }, []);
   return <h1 className="text-3xl font-bold">Hello world!</h1>;
 }
 
